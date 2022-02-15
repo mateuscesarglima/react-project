c99b29daf55146ab7b2f15e124736cba66c754a1
Mateus Cesar <mateuscesarglima@gmail.com>
2022-02-15 17:34:59 -0300

    Backend

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,15 +1,38 @@
-
-import ProjectForm from '../project/ProjectForm'
-import styles from './NewProject.module.css'
+import {useNavigate} from 'react-router-dom'
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css";
 
 const NewProject = () => {
-    return(
-        <div className={styles.newproject_container}>
-            <h1>Criar Projeto</h1>
-            <p>Crie seu projeto para depois adicionar os serviços </p>
-            <ProjectForm btnText='Create Project'/>
-        </div>
-    )
-}
+  // Permite fazer redirects na página do sistema
+  const navigate = useNavigate()
 
-export default NewProject
+  const createPost = (project) => {
+    //initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        navigate('/project')
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços </p>
+      <ProjectForm handleSubmit={createPost} btnText="Create Project" />
+    </div>
+  );
+};
+
+export default NewProject;
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -4,39 +4,69 @@ import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 import styles from "./ProjectForm.module.css";
 
-const ProjectForm = ({ btnText }) => {
+const ProjectForm = ({ handleSubmit, btnText, projectData }) => {
   const [categories, setCategories] = useState([]);
-
+  const [project, setProject] = useState(projectData || {});
   useEffect(() => {
     fetch("http://localhost:5000/categories", {
-    method: "GET",
-    header: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((resp) => resp.json())
-    .then((data) => {
-      setCategories(data)
+      method: "GET",
+      header: {
+        "Content-Type": "application/json",
+      },
     })
-    .catch((err) => console.log(err));
-  }, [])
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const submit = (e) => {
+    // Não permite que a pagina seja feita através de page reload
+    e.preventDefault();
+    handleSubmit(project);
+  };
+
+  function handleChange (e){
+    setProject({ ...project, [e.target.name]: e.target.value });
+  };
+
+  function handleCategory(e){
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+      },
+    });
+  };
 
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Project name"
         name="name"
         placeholder="Enter project name"
+        handleOnChange={handleChange}
+        value={project.name ? project.name : ""}
       />
       <Input
         type="number"
         text="Project Budget"
         name="budget"
         placeholder="Total budget"
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget : ""}
       />
 
-      <Select name="category_id" text="Select category" options={categories} />
+      <Select
+        name="category_id"
+        text="Select category"
+        options={categories}
+        handleOnChange={handleCategory}
+        value={project.category ? project.category.id : ""}
+      />
 
       <SubmitButton text={btnText} />
     </form>
